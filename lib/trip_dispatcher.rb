376cf455f600caaa6a916c2cc1df3488c026d798
Phoebe <phoebe.manning8@gmail.com>
2018-02-27 14:39:38 -0800

    adds Time.parse to time vars in load_trips

--- lib/trip_dispatcher.rb
+++ lib/trip_dispatcher.rb
@@ -1,5 +1,6 @@
 require 'csv'
 require 'time'
+require 'pry'
 
 require_relative 'driver'
 require_relative 'passenger'
@@ -75,8 +76,8 @@ module RideShare
           id: raw_trip[:id].to_i,
           driver: driver,
           passenger: passenger,
-          start_time: raw_trip[:start_time],
-          end_time: raw_trip[:end_time],
+          start_time: Time.parse(raw_trip[:start_time]),
+          end_time: Time.parse(raw_trip[:end_time]),
           cost: raw_trip[:cost].to_f,
           rating: raw_trip[:rating].to_i
         }
