419ea4b8c363bcbb2ccd4b869a06dd16d383ad58
Wenjie <traxiaojie@gmail.com>
2018-02-27 21:39:22 -0800

    Change before do block to a upper level so other tests can also access Add tests for end time is earlier than start time and duration method

--- specs/trip_spec.rb
+++ specs/trip_spec.rb
@@ -1,23 +1,23 @@
 require_relative 'spec_helper'
 
 describe "Trip class" do
+  before do
+    start_time = Time.parse('2015-05-20T12:14:00+00:00')
+    end_time = start_time + 25 * 60 # 25 minutes
+    @trip_data = {
+      id: 8,
+      driver: RideShare::Driver.new(id: 3, name: "Lovelace", vin: "12345678912345678"),
+      passenger: RideShare::Passenger.new(id: 1, name: "Ada", phone: "412-432-7640"),
+      start_time: start_time,
+      end_time: end_time,
+      cost: 23.45,
+      rating: 3
+    }
+    # binding.pry
+    @trip = RideShare::Trip.new(@trip_data)
+  end
 
   describe "initialize" do
-    before do
-      start_time = Time.parse('2015-05-20T12:14:00+00:00')
-      end_time = start_time + 25 * 60 # 25 minutes
-      @trip_data = {
-        id: 8,
-        driver: RideShare::Driver.new(id: 3, name: "Lovelace", vin: "12345678912345678"),
-        passenger: RideShare::Passenger.new(id: 1, name: "Ada", phone: "412-432-7640"),
-        start_time: start_time,
-        end_time: end_time,
-        cost: 23.45,
-        rating: 3
-      }
-      @trip = RideShare::Trip.new(@trip_data)
-    end
-
     it "is an instance of Trip" do
       @trip.must_be_kind_of RideShare::Trip
     end
@@ -38,5 +38,24 @@ describe "Trip class" do
         }.must_raise ArgumentError
       end
     end
+
+    it "raises an error if end time is before or as same as the start time" do
+      start_time = Time.parse('2015-05-20T12:14:00+00:00')
+      end_times = [start_time - 10, start_time - 5 * 60]
+      end_times.each do |end_time|
+        @trip_data[:end_time] = end_time
+        proc {
+          RideShare::Trip.new(@trip_data)
+        }.must_raise ArgumentError
+      end
+    end
+  end  # initializer ends
+
+  describe "duration method" do
+    it "returns the duration time of a trip" do
+      @trip.duration.must_equal 25 * 60.to_f
+    end
   end
+
+
 end
