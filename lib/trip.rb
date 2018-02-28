c27491ccc1aaae632b8607f4eafdbf94ebe7cf55
Anne <annehwatson@gmail.com>
2018-02-27 16:06:00 -0800

    changed start_time and end_time to Time.new

--- lib/trip.rb
+++ lib/trip.rb
@@ -1,4 +1,5 @@
 require 'csv'
+require 'time'
 
 module RideShare
   class Trip
@@ -9,9 +10,17 @@ module RideShare
       @driver = input[:driver]
       @passenger = input[:passenger]
       @start_time = input[:start_time]
+      #utc no offset?
       @end_time = input[:end_time]
+      #utc no offset?
       @cost = input[:cost]
+      #float
       @rating = input[:rating]
+      #(1..5)
+
+      if end_time < start_time
+        raise ArgumentError.new("Trip End Time cannot be before Trip Start Time")
+      end
 
       if @rating > 5 || @rating < 1
         raise ArgumentError.new("Invalid rating #{@rating}")
@@ -19,3 +28,7 @@ module RideShare
     end
   end
 end
+
+# trip_input = {id: 1,driver: 12, passenger: 13, start_time: "2017-04-05T14:01:00+00:00", end_time: "2016-04-05T14:01:00+00:00", cost: 20.00, rating: 5}
+# my_trip = RideShare::Trip.new(trip_input)
+# puts my_trip
