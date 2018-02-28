e9f8c0da16b97b252e0cc87fc488ca48373a8734
Madaleine Shields <maddiev.shields@gmail.com>
2018-02-27 18:51:29 -0800

    minor change to wave 1.1

--- lib/trip.rb
+++ lib/trip.rb
@@ -17,13 +17,7 @@ module RideShare
         raise ArgumentError.new("Invalid rating #{@rating}")
       end
 
-      if @end_time.to_f < @start_time.to_f
-        # p "+++++TEST+++"
-        # p @start_time
-        # p @end_time
-        #
-        # p @start_time.to_f
-        # p @end_time.to_f
+      if @end_time < @start_time
 
         raise ArgumentError.new("Start time must be before end time.")
 
@@ -31,8 +25,8 @@ module RideShare
     end
 
     def duration
-      @duration = @end_time - @start_time
-      return @duration
+      duration = @end_time - @start_time
+      return duration
     end
 
   end
