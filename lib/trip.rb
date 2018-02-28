891073b78d421201f1c46ef5c845368845cf0637
Brittany Jones <brittany.r.jones@hotmail.com>
2018-02-28 14:48:43 -0800

    wrote code for duration, currently passing, need to fix instance vars and integer conversions

--- lib/trip.rb
+++ lib/trip.rb
@@ -2,7 +2,7 @@ require 'csv'
 
 module RideShare
   class Trip
-    attr_reader :id, :passenger, :driver, :start_time, :end_time, :cost, :rating
+    attr_reader :id, :passenger, :driver, :start_time, :end_time, :cost, :rating, :duration
 
     def initialize(input)
       @id = input[:id]
@@ -12,6 +12,7 @@ module RideShare
       @end_time = input[:end_time]
       @cost = input[:cost]
       @rating = input[:rating]
+      @duration = input [:duration]
 
       if @rating > 5 || @rating < 1
         raise ArgumentError.new("Invalid rating #{@rating}")
@@ -20,7 +21,14 @@ module RideShare
       if @start_time.to_i > @end_time.to_i
         raise ArgumentError.new("Start time must be before end time.")
       end
-
     end
+
+    def duration
+      @duration = (@end_time.to_i - @start_time.to_i)
+      return @duration
+    end
+
+
+
   end
 end
