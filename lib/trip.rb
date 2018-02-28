cdb0a886c2ebdcadb172c8d55529cc438dccd5d5
Anne <annehwatson@gmail.com>
2018-02-27 21:01:01 -0800

    what is time

--- lib/trip.rb
+++ lib/trip.rb
@@ -25,14 +25,18 @@ module RideShare
     end
 
     def calculate_duration
-      end_time = Time.parse(@end_time)
-      # puts end_time.to_a
-      start_time = Time.parse(@start_time)
-      return end_time - start_time #for 1 hour trip, returns 3600.0 seconds (60 seconds * 60 minutes)
+      return @end_time - @start_time
+      # end_time = Time.parse(@end_time)
+      # # puts end_time.to_a
+      # start_time = Time.parse(@start_time)
+      # return end_time - start_time #for 1 hour trip, returns 3600.0 seconds (60 seconds * 60 minutes)
     end
   end
 end
 
+trip_start_time = Time.new(2017, 04, 05, 8, 0, 1, "-00:00")
+trip_end_time = Time.new(2017, 04, 05, 9, 0, 1, "-00:00")
+trip_input = {id: 1,driver: 12, passenger: 13, start_time: trip_start_time, end_time: trip_end_time, cost: 20.00, rating: 5}
 # trip_input = {id: 1,driver: 12, passenger: 13, start_time: "2017-04-05T08:01:00+00:00", end_time: "2017-04-05T09:01:00+00:00", cost: 20.00, rating: 5}
-# my_trip = RideShare::Trip.new(trip_input)
-# puts my_trip.calculate_duration
+my_trip = RideShare::Trip.new(trip_input)
+puts my_trip.calculate_duration
