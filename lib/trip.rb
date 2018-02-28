cbcd6588d236121cfbbf31e733e602389680a9a9
Wenjie <traxiaojie@gmail.com>
2018-02-27 21:32:39 -0800

    Update date by using Time#parse

--- lib/trip.rb
+++ lib/trip.rb
@@ -1,4 +1,5 @@
 require 'csv'
+require "time"
 
 module RideShare
   class Trip
@@ -8,14 +9,23 @@ module RideShare
       @id = input[:id]
       @driver = input[:driver]
       @passenger = input[:passenger]
-      @start_time = input[:start_time]
-      @end_time = input[:end_time]
+      @start_time = input[:start_time].class == Time ? input[:start_time] : Time.parse(input[:start_time]) # instance of Time
+      @end_time = input[:end_time] # instance of Time
       @cost = input[:cost]
       @rating = input[:rating]
 
       if @rating > 5 || @rating < 1
         raise ArgumentError.new("Invalid rating #{@rating}")
       end
+
+      if !@end_time.nil? && @start_time > @end_time
+        # binding.pry
+        raise ArgumentError.new("Invalid time")
+      end
+    end
+
+    def duration
+      @end_time - @start_time
     end
   end
 end
