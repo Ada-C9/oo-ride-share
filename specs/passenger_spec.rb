3aaa0930a0e9ddb684ef9e9f76c1de7992db2abc
Angelica Maria Ceja Morales <amcejamorales@gmail.com>
2018-02-27 23:51:15 -0800

    Create and test functionality to return total amount of money spent by a passenger

--- specs/passenger_spec.rb
+++ specs/passenger_spec.rb
@@ -1,4 +1,5 @@
 require_relative 'spec_helper'
+require 'time'
 
 describe "Passenger class" do
 
@@ -6,7 +7,7 @@ describe "Passenger class" do
     before do
       @passenger = RideShare::Passenger.new({id: 1, name: "Smithy", phone: "353-533-5334"})
     end
- 
+
     it "is an instance of Passenger" do
       @passenger.must_be_kind_of RideShare::Passenger
     end
@@ -73,5 +74,31 @@ describe "Passenger class" do
         driver.must_be_kind_of RideShare::Driver
       end
     end
+  end # describe get_drivers method
+
+  describe "total_spent" do
+    before do
+      @passenger = RideShare::Passenger.new(id: 9, name: "Merl Glover III", phone: "1-602-620-2330 x3723", trips: [])
+      @trip = RideShare::Trip.new({id: 8, driver: nil, passenger: @passenger, start_time: Time.parse('2015-05-20T12:14:00+00:00'), end_time: Time.parse('2015-05-20T12:25:00+00:00'), cost: 10.15, rating: 5})
+      @passenger.add_trip(@trip)
+    end
+
+    it "returns a float equal to amount passenger has spent on all trips" do
+      trip_hundred = RideShare::Trip.new({id: 100, driver: nil, passenger: @passenger, start_time: Time.parse('2014-05-20T12:14:00+00:00'), end_time: Time.parse('2014-05-20T12:25:00+00:00'), cost: 5.20, rating: 5})
+
+      trip_hundred_one = RideShare::Trip.new({id: 101, driver: nil, passenger: @passenger, start_time: Time.parse('2013-05-20T12:14:00+00:00'), end_time: Time.parse('2013-05-20T12:25:00+00:00'), cost: 8.54, rating: 5})
+
+      @passenger.add_trip(trip_hundred)
+      @passenger.add_trip(trip_hundred_one)
+
+      total_spent = @trip.cost + trip_hundred.cost + trip_hundred_one.cost
+
+      result = @passenger.total_spent
+
+      result.must_be_kind_of Float
+      result.must_equal total_spent
+
+    end
   end
-end
+
+end # describe Passenger
