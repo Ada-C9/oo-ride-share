7d2040fc9d01fe7d77af4722be6d341da5c397f4
Brittany Jones <brittany.r.jones@hotmail.com>
2018-02-28 12:57:33 -0800

    created test to calculate duration of trip. test not passing

--- specs/trip_spec.rb
+++ specs/trip_spec.rb
@@ -42,7 +42,8 @@ describe "Trip class" do
     it 'Raises ArgumentError if start time is after end time' do
 
       start_time = Time.parse('2015-05-20T12:14:00+00:00')
-      end_time = start_time - 25 * 60 # - 25 minutes
+      end_time = start_time - 1 * 60
+      #try for 0 and 1 minutes
 
       @trip_data = {
         id: 8,
@@ -57,6 +58,29 @@ describe "Trip class" do
       proc {RideShare::Trip.new(@trip_data)}.must_raise ArgumentError
     end
   end
+  describe "Duration of trip" do
+    it 'Calculates the duration of trip in seconds' do
+      start_time = Time.parse('2015-05-20T12:14:00+00:00')
+      end_time = start_time + 1 * 60
+      #One minute times 60 to get 60 seconds
+
+      @trip_data = {
+        id: 8,
+        driver: RideShare::Driver.new(id: 3, name: "Lovelace", vin: "12345678912345678"),
+        passenger: RideShare::Passenger.new(id: 1, name: "Ada", phone: "412-432-7640"),
+        start_time: start_time,
+        end_time: end_time,
+        cost: 23.45,
+        rating: 3
+      }
+
+      @trip = RideShare::Trip.new(@trip_data)
+      @trip.duration_of_trip.must_equal 60
+    end
+  end
+
+
+
 
 
 end
