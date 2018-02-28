d78d5e880f30f320454d592534413e212e353168
Monalisa Chatterjee <usermonalisa@gmail.com>
2018-02-28 11:57:04 -0800

    Add test for accurately loading last trip, all tests passing.

--- specs/trip_dispatch_spec.rb
+++ specs/trip_dispatch_spec.rb
@@ -93,19 +93,26 @@ describe "TripDispatcher class" do
       dispatcher = RideShare::TripDispatcher.new
       first_trip = dispatcher.trips.first
       last_trip = dispatcher.trips.last
-      #attr_reader :id, :passenger, :driver, :start_time, :end_time, :cost, :rating
-    #  1, 1, 54, 2016-04-05T14:01:00+00:00, 2016-04-05T14:09:00+00:00, 17.39, 3
 
       first_trip.id.must_equal 1
       first_trip.driver.id.must_equal 1
       first_trip.passenger.id.must_equal 54
       first_trip.start_time.must_equal Time.parse('2016-04-05T14:01:00+00:00')
+      first_trip.start_time.must_be_instance_of Time
       first_trip.end_time.must_equal Time.parse('2016-04-05T14:09:00+00:00')
+      first_trip.end_time.must_be_instance_of Time
       first_trip.cost.must_equal 17.39
       first_trip.rating.must_equal 3
 
-      # last_trip.name.must_equal "Miss Isom Gleason"
-      # last_trip.id.must_equal 300
+      last_trip.id.must_equal 600
+      last_trip.driver.id.must_equal 61
+      last_trip.passenger.id.must_equal 168
+      last_trip.start_time.must_be_instance_of Time
+      last_trip.start_time.must_equal Time.parse('2016-04-25T02:59:00+00:00')
+      last_trip.end_time.must_be_instance_of Time
+      last_trip.end_time.must_equal Time.parse('2016-04-25T03:06:00+00:00')
+      last_trip.cost.must_equal 26.76
+      last_trip.rating.must_equal 3
     end
 
   end
