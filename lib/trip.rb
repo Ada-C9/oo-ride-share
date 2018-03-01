130bf214da4d2e810f28fc08829873b69f90b606
SelamawitA <Selamawit.ainalem@gmail.com>
2018-02-28 18:13:54 -0800

    extended Argument error for @rating,@end_time, and @start times to allow for :PENDING status

--- lib/trip.rb
+++ lib/trip.rb
@@ -2,6 +2,7 @@ require 'csv'
 
 module RideShare
   class Trip
+    PENDING_TRIP = 0
     attr_reader :id, :passenger, :driver, :start_time, :end_time, :cost, :rating
 
     def initialize(input)
@@ -12,21 +13,23 @@ module RideShare
       @end_time = input[:end_time]
       @cost = input[:cost]
       @rating = input[:rating]
-
-      if @start_time.to_i > @end_time.to_i
+      #@start_time.class != Symbol && @end_time.class != Symbol
+      if @start_time != :PENDING && @end_time != :PENDING && @start_time  > @end_time
         raise ArgumentError.new('invald time input')
       end
 
-      if @rating > 5 || @rating < 1
+      if @rating != :PENDING && (@rating > 5 || @rating < 1)
         raise ArgumentError.new("Invalid rating #{@rating}")
       end
     end
 
     def trip_in_seconds
+      if end_time == :PENDING && start_time = :PENDING
+        return PENDING_TRIP
+      else
       return (end_time.to_i - start_time.to_i)
     end
-
-
+    end
 
   end
 end
