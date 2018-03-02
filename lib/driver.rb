724e712f5eee3140103b3f1eff7163d0ffd6307d
Kirsten Schumy <kirsten.schumy@gmail.com>
2018-03-02 11:16:14 -0800

    removed trip duration summing, changed these method calls to go to rideshare module, and added comments

--- lib/driver.rb
+++ lib/driver.rb
@@ -14,7 +14,7 @@ module RideShare
       @name = RideShare.return_valid_name_or_error(input[:name])
       @vehicle_id = input[:vin]
       @status = input[:status] == nil ? :AVAILABLE : input[:status]
-      @trips = input[:trips] == nil ? [] : RideShare.valid_trips_or_errors(input[:trips])
+      @trips = input[:trips] == nil ? [] : RideShare.return_valid_trips_or_errors(input[:trips])
     end
 
     def get_average_rating # TODO: TEST!! # test if only trip is incomplete
@@ -22,16 +22,21 @@ module RideShare
       return num_of_trips == 0 ? 0 : (get_ratings.to_f / num_of_trips)
     end
 
+    # Throws ArgumentError is provided trip is not a trip or if the trip is in
+    # progress but the driver is unavailable.
+    # Adds trip to trips and sets status to unavailable if trip is in progress.
     def add_trip(trip)
       RideShare.return_valid_trip_or_error(trip)
       check_and_update_status if trip.is_in_progress?
       @trips << trip
     end
 
+    # Returns the total revenue of all completed trips.
     def get_total_revenue # TODO: TEST!!
       return calculate_total_revenue #@trips.inject(0.0) { |total, trip| total + (trip.cost - 1.56) * 0.80 }
     end
 
+    # Returns the total revenue of all completed trips.
     def get_avg_revenue_per_hour # TODO: TEST!! # test if only trip is incomplete
       return (get_total_revenue / get_all_trip_durations).round(2)
     end
@@ -56,35 +61,21 @@ module RideShare
     end
 
     def get_all_trip_durations
-      return @trips.inject(0) do |sum, trip|
-        sum + trip.get_duration if !trip.is_in_progress?
-      end.to_f / 120
+      RideShare.get_all_trip_durations_in_seconds(trips).to_f / 120
     end
 
+    #
     def get_ratings
       return @trips.inject(0) do |sum, trip|
         sum + trip.rating if !trip.is_in_progress?
       end
     end
 
+    # Returns the number of completed trips.
     def get_num_of_completed_trips
-      return @trips.count { |trip| !trip.is_in_progress? }
+      return @trips.length - 1 if !@trips.empty? && @trips.last.is_in_progress?
+      return @trips.length
     end
 
   end
 end
-
-
-# original average rating
-# total_ratings = 0
-# @trips.each do |trip|
-#   total_ratings += trip.rating
-# end
-#
-# if trips.length == 0
-#   average = 0
-# else
-#   average = (total_ratings.to_f) / trips.length
-# end
-#
-# return average
--- lib/passenger.rb
+++ lib/passenger.rb
@@ -8,25 +8,29 @@ module RideShare
       @id = RideShare.return_valid_id_or_error(input[:id])
       @name = RideShare.return_valid_name_or_error(input[:name])
       @phone_number = input[:phone]
-      @trips = input[:trips] == nil ? [] : RideShare.valid_trips_or_errors(input[:trips])
+      @trips = input[:trips] == nil ? [] : RideShare.return_valid_trips_or_errors(input[:trips])
     end
 
+    
     def get_drivers
       @trips.map{ |trip| trip.driver }
     end
 
+    # Throws ArgumentError if provided trip is not a valid Trip. Otherwise, adds
+    # trip to trips.
     def add_trip(trip)
       @trips << RideShare.return_valid_trip_or_error(trip)
     end
 
+    # Returns the
     def get_total_money_spent
       return @trips.inject(0.0) { |sum, trip| sum + trip.cost if
         !trip.is_in_progress? }
     end
 
+    # Returns the sum of all complete trips durations in seconds.
     def get_total_time
-      return @trips.inject(0) { |sum, trip| sum + trip.get_duration if
-        !trip.is_in_progress?}
+      return RideShare.get_all_trip_durations_in_seconds(@trips)
     end
 
     private
