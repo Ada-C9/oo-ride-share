12c151848f82081b64127c1540946e54dc52b198
CheerOnMars <CheerOnMars+gh@posteo.org>
2018-03-02 16:58:03 -0800

    format test

--- specs/driver_spec.rb
+++ specs/driver_spec.rb
@@ -117,7 +117,7 @@ describe "Driver class" do
       trip1 = RideShare::Trip.new({id: 8, driver: @driver, passenger: @passenger, start_time: Time.parse('2015-05-20T12:14:00+00:00'), end_time: Time.parse('2015-05-20T12:24:00+00:00'), cost: 11.65, rating: 5})
 
       trip2 = RideShare::Trip.new({id: 18, driver: @driver, passenger: @passenger, start_time: Time.parse('2015-04-20T12:14:00+00:00'), end_time: Time.parse('2015-04-20T12:24:00+00:00'), cost: 12.65, rating: 5})
-
+      
       trip3 = RideShare::Trip.new({id: 28, driver: @driver, passenger: @passenger, start_time: Time.parse('2015-07-20T12:14:00+00:00'), end_time: Time.parse('2015-07-20T12:19:00+00:00'), cost: 5.65, rating: 5})
 
       @driver.add_trip(trip1)
@@ -132,9 +132,5 @@ describe "Driver class" do
     end
 
   end
-
-
-
-
 
 end
