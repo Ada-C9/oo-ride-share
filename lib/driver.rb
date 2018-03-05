a7a9f854190a000d55ac101996a52f46b320cf17
SelamawitA <Selamawit.ainalem@gmail.com>
2018-03-05 01:08:11 -0800

    added condition to total_revenu to account for pending trips

--- lib/driver.rb
+++ lib/driver.rb
@@ -16,12 +16,10 @@ module RideShare
       if input[:vin] == nil || input[:vin].length != 17
         raise ArgumentError.new("VIN cannot be less than 17 characters.  (got #{input[:vin]})")
       end
-
       @id = input[:id]
       @name = input[:name]
       @vehicle_id = input[:vin]
       @status = input[:status] == nil ? :AVAILABLE : input[:status]
-
       @trips = input[:trips] == nil ? [] : input[:trips]
     end
 
@@ -30,13 +28,11 @@ module RideShare
       @trips.each do |trip|
         total_ratings += trip.rating
       end
-
       if trips.length == 0
         average = 0
       else
         average = (total_ratings.to_f) / trips.length
       end
-
       return average
     end
 
@@ -44,7 +40,6 @@ module RideShare
       if trip.class != Trip
         raise ArgumentError.new("Can only add trip instance to trip collection")
       end
-
       @trips << trip
     end
 
@@ -52,27 +47,22 @@ module RideShare
       total_revenue = 0
       @trips.each do |a_trip|
         total_revenue += (a_trip.cost - DRIVER_FEE)*PERCENT_ALLOCATED_TO_DRIVER
+      end
+      return total_revenue
     end
 
-    return total_revenue
-  end
-
-  ##UPDATE HERE TO  INCLUDE CONDITION FOR PENDING TRIPS ##
-  def total_revenue_per_hour
-    # if total time = 0
-    # return total time
-    #you'll need to test for this condition and refactor.
+    def total_revenue_per_hour
       total_time = 0
       @trips.each do |a_trip|
         total_time += a_trip.trip_in_seconds
       end
-      #test for less than an hour/compensate for less than an hour
-      total_time = (total_time.to_f/SECONDS_TO_HOUR_RATE)
-
-      rev_per_hour = total_revenue/(total_time)
+      if total_time == 0
+        rev_per_hour = 0
+      else
+        total_time = (total_time.to_f/SECONDS_TO_HOUR_RATE)
+        rev_per_hour = total_revenue/(total_time)
+      end
       return rev_per_hour
-
+    end
   end
-
-end
 end
