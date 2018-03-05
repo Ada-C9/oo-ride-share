6c130bf95836231649f16545c8a989d257884820
SelamawitA <Selamawit.ainalem@gmail.com>
2018-03-05 01:04:17 -0800

    added tests for multiple trips on total amount of time spent on trip method and total amount spent method

--- specs/passenger_spec.rb
+++ specs/passenger_spec.rb
@@ -58,7 +58,6 @@ describe "Passenger class" do
   end
 
   describe "total_amount_spent" do
-    #no trips, multiple trips tests include these!
     it "can return the total amount spent on rides per passenger" do
       passenger = RideShare::Passenger.new(id: 9, name: "Merl Glover III", phone: "1-602-620-2330 x3723")
       driver = RideShare::Driver.new(id: 3, name: "Lovelace", vin: "12345678912345678")
@@ -71,13 +70,58 @@ describe "Passenger class" do
       passenger.total_amount_spent.must_equal 250
 
     end
+
+    it "can return the total amount spent after multiple trips have been taken" do
+      passenger = RideShare::Passenger.new(id: 9, name: "Merl Glover III", phone: "1-602-620-2330 x3723")
+      driver = RideShare::Driver.new(id: 3, name: "Lovelace", vin: "12345678912345678")
+      start_time = Time.parse('2015-05-20T12:14:00+00:00')
+      end_time = start_time + (1500)
+      second_trip_start_time = Time.parse('2016-05-20T12:14:00+00:00')
+      second_trip_end_time = second_trip_start_time + (1500)
+
+      trip = RideShare::Trip.new({id: 8, driver: driver, passenger: passenger, start_time: start_time, end_time:end_time ,rating: 5,cost:10})
+      second_trip = RideShare::Trip.new({id: 8, driver: driver, passenger: passenger, start_time: second_trip_start_time, end_time:second_trip_end_time ,rating: 5,cost:10})
+      passenger.add_trip(trip)
+      passenger.add_trip(second_trip)
+      passenger.total_amount_spent.must_equal 500
+
+    end
   end
 
   describe "total_amount_time" do
     it "can return the total amount of time spend on a ride" do
-      a_trip = RideShare::Trip.new({id:12345,driver:"A driver",passenger:"a passenger",start_time:0,end_time:60,cost:10,rating:4})
-      a_passenger = RideShare::Passenger.new({id: 9, name: "Merl Glover III", phone: "1-602-620-2330 x3723", trips: [a_trip]})
+      a_passenger = RideShare::Passenger.new(id: 9, name: "Merl Glover III", phone: "1-602-620-2330 x3723")
+      start_time = Time.parse('2018-03-02 04:00:18 +0000')
+      end_time = Time.parse('2018-03-02 05:00:18 +0000')
+      a_trip = RideShare::Trip.new({id:12345,driver:"A driver",passenger:"a passenger",start_time:start_time,end_time:end_time,cost:10,rating:4})
+      a_passenger.add_trip(a_trip)
+
       a_passenger.total_amount_time.must_equal 1
+    end
+
+    it "can return total amount of time spent on a ride after multiple trips" do
+      a_passenger = RideShare::Passenger.new(id: 9, name: "Merl Glover III", phone: "1-602-620-2330 x3723")
+      start_time = Time.parse('2018-03-02 04:00:18 +0000')
+      end_time = Time.parse('2018-03-02 05:00:18 +0000')
+
+      second_start = Time.parse('2018-03-02 07:00:18 +0000')
+      second_end = Time.parse('2018-03-02 8:00:18 +0000')
+
+      third_start = Time.parse('2018-03-02 09:00:18 +0000')
+      third_end = Time.parse('2018-03-02 10:00:18 +0000')
+
+      a_trip = RideShare::Trip.new({id:12345,driver:"A driver",passenger:"a passenger",start_time:start_time,end_time:end_time,cost:10,rating:4})
+      a_passenger.add_trip(a_trip)
+      second_trip = RideShare::Trip.new({id:12345,driver:"A driver",passenger:"a passenger",start_time:second_start,end_time:second_end,cost:10,rating:4})
+      a_passenger.add_trip(a_trip)
+      third_trip = RideShare::Trip.new({id:12345,driver:"A driver",passenger:"a passenger",start_time:third_start,end_time:third_end,cost:10,rating:4})
+      a_passenger.add_trip(a_trip)
+
+      a_passenger.add_trip(a_trip)
+      a_passenger.add_trip(second_trip)
+      a_passenger.add_trip(third_trip)
+      a_passenger.total_amount_time.must_equal 6
+
     end
   end
 
