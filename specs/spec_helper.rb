e277a742526ac3026e60de6a2df7f51da000ec52
Kate Pond <kate@thepondsedge.com>
2018-03-05 10:21:28 -0800

    moves SimpleCov to the correct location in spec_helper

--- specs/spec_helper.rb
+++ specs/spec_helper.rb
@@ -1,10 +1,10 @@
 require 'time'
+require 'simplecov'
+SimpleCov.start
+
 require 'minitest'
 require 'minitest/autorun'
 require 'minitest/reporters'
-
-require 'simplecov'
-SimpleCov.start
 
 Minitest::Reporters.use! Minitest::Reporters::SpecReporter.new
 
