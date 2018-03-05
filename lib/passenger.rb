4f1bb32868bef07af24fa9ed870e99d219d4a8d3
Luxi Lindsey <Luxi.Lindsey@gmail.com>
2018-03-04 22:54:06 -0800

    Updated passenger.rb
    I removed the extra comments.

--- lib/passenger.rb
+++ lib/passenger.rb
@@ -12,13 +12,6 @@ module RideShare
       @name = input[:name]
       @phone_number = input[:phone]
 
-      # Did whoever gave us this input hash put any trip in it?
-      # if input.keys.include? :trips => would be pretty much the same syntax
-      #   @trips = input[:trips]
-      # else
-      #   @trips = []
-      # end
-      # above is the same as below
       @trips = input[:trips] == nil ? [] : input[:trips]
     end
 
@@ -52,18 +45,3 @@ module RideShare
 
   end # end of Passenger class
 end # end of RideShare module
-
-# testing_code = RideShare::Passenger.new(
-# id: 21,
-# name: "Lovelace",
-# vin: "12345678912345678"
-# trips: = RideShare::Trip.new({
-# id: 21,
-# driver: "Luxi",
-# passenger: "Mark",
-# start_time: Time.parse('2015-05-20T12:14:00+00:00'),
-# end_time: Time.parse('2015-05-20T12:14:30+00:00'),
-# cost: 25.00,
-# rating: 3}))
-#
-# puts testing_code.money_spent
