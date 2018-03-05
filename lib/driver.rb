719f85441e1cea36f0cd45d9b344026fc5a286c6
amanzan1 <anna.manzanares7683@gmail.com>
2018-03-04 19:40:43 -0800

    finished second half requirements wave 2

--- lib/driver.rb
+++ lib/driver.rb
@@ -49,7 +49,7 @@ module RideShare
       if trip.class != Trip
         raise ArgumentError.new("Can only add trip instance to trip collection")
       end
-
+      @status = :UNAVAILABLE
       @trips << trip
     end
   end
